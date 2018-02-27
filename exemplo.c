6baa0cb3e5268199507ee36b7af57ca8646daa01
Rui Mendes <ruifilipe29mendes@gmail.com>
2018-02-27 22:37:28 +0000

    Tentativa de Limpeza
    Try
    
    Simplificações e introdução do scale do tabuleiro
    
    Mudança do getScaleFactor para função
    
    Fixing center

--- exemplo.c
+++ exemplo.c
@@ -14,6 +14,8 @@ Esqueleto do programa
 #define TAM 40
 #define ECRA_X 1904
 #define ECRA_Y 938
+#define DTAB 800
+#define MUDA_SEGUINTE(I,J,E) E.grelha[I][J] = (E.grelha[I][J]==5) ? 3 : ((E.grelha[I][J])+1)
 
 /**
 Função que inicializa o estado
@@ -34,6 +36,11 @@ ESTADO inicializar(int nl, int nc) {
   return e;
 }
 
+void getScaleFactor(int * sf, ESTADO e)
+{
+  *sf= (e.num_lins >= e.num_cols) ? (float) (DTAB/e.num_lins) : (float) (DTAB/e.num_cols);
+}
+
 /**
 Lê o estado a partir da variável de ambiente QUERY_STR. Caso não seja passado um valor, chama a função inicializar
 @param args O valor da variável (o que é passado depois de ? no URL)
@@ -41,8 +48,30 @@ Lê o estado a partir da variável de ambiente QUERY_STR. Caso não seja passado
 */
 ESTADO ler_estado(char *args) {
   if(strlen(args) == 0)
-  return inicializar(3,3);
+  return inicializar(12,12);
   return str2estado(args);
+}
+
+/**
+Coloca a imagem correspondente no local indicado
+@param A posição x e y onde a imagem vai ser colocada assim como um char para identificar qual será essa imagem
+*/
+void drawPeca (int i,int j,char x,ESTADO e,int sf)
+{
+  int cx = ((ECRA_X-DTAB)/2)-1,cy = ((ECRA_Y-DTAB)/2)-1,fnrx,fnry;//fnr = fix non rect
+  fnrx = (DTAB - (e.num_lins*sf))/2;
+  fnry = (DTAB -(e.num_cols*sf))/2;
+  char s [50];
+  switch (x)
+  {
+    case VAZIA : strcpy(s,"vazio.png");break;
+    case BLOQUEADA : strcpy(s,"bloq.png");break;
+    case FIXO_X :
+    case SOL_X  : strcpy(s,"X.png");break;
+    case FIXO_O :
+    case SOL_O : strcpy(s,"O.png");break;
+  }
+  IMAGEM(i+cx+fnrx,j+cy+fnry,sf,s);
 }
 
 /**
@@ -50,31 +79,24 @@ Função principal do programa
 @returns 0 se tudo correr bem
 */
 int main() {
-ESTADO e= ler_estado(getenv("QUERY_STRING"));
-int j, i;
-
+ESTADO e = ler_estado(getenv("QUERY_STRING"));
+int j, i, sf;
+char holder;
+getScaleFactor(&sf,e);
 COMECAR_HTML;
   ABRIR_SVG(ECRA_X, ECRA_Y, "#000");
     for(i=0;i<e.num_lins;i++){
       for(j=0;j<e.num_cols;j++){
-        switch(e.grelha[i][j]){
-          case VAZIA : e.grelha[i][j]=SOL_X;
-                       ABRIR_LINK(estado2str(e));
-                       e.grelha[i][j]=VAZIA;
-                       IMAGEM(i,j,40, "vazio.png"); break;
-          case BLOQUEADA: IMAGEM(i,j,40, "bloq.png"); break;
-          case FIXO_X: IMAGEM(i,j,40, "X.png"); break;
-          case SOL_X:  e.grelha[i][j]=SOL_O;
-                       ABRIR_LINK(estado2str(e));
-                       e.grelha[i][j]=SOL_X;
-                       IMAGEM(i,j,40, "X.png"); break;
-          case FIXO_O: IMAGEM(i,j,40, "O.png"); break;
-          case SOL_O:  e.grelha[i][j]=VAZIA;
-                       ABRIR_LINK(estado2str(e));
-                       e.grelha[i][j]=SOL_O;
-                       IMAGEM(i,j,40, "O.png"); break;
+        if (e.grelha[i][j]>FIXO_X)
+        {
+          holder = e.grelha[i][j];
+          MUDA_SEGUINTE(i,j,e);
+          ABRIR_LINK(estado2str(e));
+          drawPeca(i,j,holder,e,sf);
+          e.grelha[i][j] = holder;
+          FECHAR_LINK;
         }
-        FECHAR_LINK;
+        else drawPeca(i,j,e.grelha[i][j],e,sf);
       }
     }
 
